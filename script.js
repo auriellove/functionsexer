a938058ad36648ddfed43eab96a411eae9c90f88
auriellove <auriel.e.love@gmail.com>
2022-05-01 18:15:55 -0400

    created functions and calculations

---
+++ script.js
@@ -0,0 +1,27 @@
+function areaofCircle(Radius) {
+    let circle= Radius * Radius * Math.PI;
+    return circle;
+}
+
+areaofCircle(5)
+
+function getCircumferenceofCircle(Radius) {
+    let circle= 2 * Math.PI * Radius;
+    return circle;
+}
+
+getCircumferenceofCircle(5)
+
+function getAreaofSquare(side) {
+    let square= side * side;
+    return square;
+}
+
+getAreaofSquare(5)
+
+function getAreaofTriangle(base, height) {
+    let area= base * height;
+    return area;
+}
+
+getAreaofTriangle(3,6)
